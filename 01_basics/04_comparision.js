475c6009ce91698ad8ad803cc7dbe65c4748c74d
Vishal <vishalyadav27100@gmail.com>
2024-12-18 01:40:46 +0530

    add 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log( 2 < 1)
+// console.log( 2<= 1)
+// console.log( 2 == 2)
+// console.log( 2 != 1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0)
+// console.log(null == true)
+// console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+console.log("2" === 2) /// checks value with datatypes
