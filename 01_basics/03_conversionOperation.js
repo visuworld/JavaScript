1e8c14f7b80b50f19524836558c93ff333455845
Vishal <vishalyadav27100@gmail.com>
2024-12-18 00:10:25 +0530

    clone repo

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+let temp = null;
+
+// "33" => 33
+// "33abc" => NaN
+//  true --> 1 , flase-->0
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let ValueInTemp =  Number(temp)
+// console.log(typeof temp)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(stringNumber)
