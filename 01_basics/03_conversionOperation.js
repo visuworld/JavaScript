7a0b47d9258cb7d613448231861e7abad5b77223
Vishal <vishalyadav27100@gmail.com>
2024-12-18 01:13:04 +0530

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,5 +20,35 @@ let temp = null;
 // console.log(booleanIsLoggedIn);
 
 let someNumber = 33
-let stringNumber = string(someNumber)
-console.log(stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// ------------------------------ Operations -------------------------------------
+
+let value = 3 ;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log("1" + 2 + 2)
+// console.log(1+ 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
