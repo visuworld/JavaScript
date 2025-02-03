058ffd5695a190235c19f93b23e214d7c96356f7
Vishal <vishalyadav27100@gmail.com>
2025-02-04 04:09:05 +0530

    DataType Summary

---
+++ 01_basics/DataTypes-summary.js
@@ -0,0 +1,17 @@
+ // Data-types (2)
+ // 1. Primitive (7) : string, Number, Boolean, null, undefined, Symbol, BigInt
+const id = Symbol('123')
+const symid = Symbol('123')
+console.log( id == symid)
+
+ // 2. (Non-Primitive / Reference ()) : Array, Object, Function
+ 
+const hero = ["saktiman", "Juniour-jee", "mangu"];  // Array
+
+let obj = {name:"vishal", age:20,}  // object
+
+let myfun = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id); // check data-type
